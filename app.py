3e14d7c1f5af8da48e7fbc0444419aff736012ce
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-12-27 09:58:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def edit_taskk():
   cur = mysql.connection.cursor()
   cur.execute("SELECT * FROM Class")
   t = cur.fetchall()
-  ca=request.form.get('category_name')
+  ca=request.form.get('category')
   print(ca)
   chrome_options = Options()
   options = Options()
